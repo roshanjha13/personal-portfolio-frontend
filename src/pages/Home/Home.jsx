0a4e342e72a6889d05fe04810e133541da6ec7b3
roshanjha31 <jharoshan618@gmail.com>
2023-09-30 23:54:12 +0530

    intro from frontend side

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import Intro from './Intro'
 
 const Home = () => {
   return (
     <div>
         <Header/>
+        <div className='bg-primary px-40'>
+          <Intro />
+        </div>
     </div>
   )
 }
