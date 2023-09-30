dcdf5daeb49b217fb12d62f073b61bfe161c761c
roshanjha31 <jharoshan618@gmail.com>
2023-09-30 20:43:17 +0530

    tailwind added in react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-white  text-center text-5xl">Roshan PortFolio Project</h1> 
+    </div>
+  )
+}
+
+export default App
