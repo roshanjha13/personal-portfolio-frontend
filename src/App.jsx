fc1c2ebb261e39982fe3ea3565aa226b5a4e0c31
roshanjha31 <jharoshan618@gmail.com>
2023-10-07 23:19:19 +0530

    Loader added in frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import { useState } from 'react';
+import Loader from './components/Header/Loader';
 
 function App() {
+  const [showLoading,setShowLoading] = useState(false)
 
   return (
       <BrowserRouter>
+      {
+        showLoading ? <Loader/> : null 
+      }
         <Routes>
           <Route  path='/' element= {<Home/>}/>
         </Routes>
---
+++ src/components/Header/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='h-screen flex items-center justify-center fixed inset-0 bg-primary'>
+        <div className="flex gap-5 text-4xl font-semibold sm:text-3xl">
+            <h1 className="text-secondary r">R</h1>
+            <h1 className="text-white k">K</h1>
+            <h1 className="text-tertiary j">J</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
