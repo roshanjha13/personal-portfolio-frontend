eee787545d58fc924cc49e307736442fe88b3739
roshanjha31 <jharoshan618@gmail.com>
2023-10-22 12:40:06 +0530

    Admin panel setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='p-5 bg-primary flex justify-between'>
-       <h1 className='text-secondary text-4xl font-semibold'>Roshan</h1> 
-       <h1 className='text-secondary text-4xl font-semibold'>Kumar</h1> 
-       <h1 className='text-secondary text-4xl font-semibold'>Jha</h1> 
+    <div className="p-5 bg-primary flex justify-between header">
+      <h1 className="text-secondary text-4xl font-semibold">Roshan</h1>
+      <h1 className="text-secondary text-4xl font-semibold">Kumar</h1>
+      <h1 className="text-secondary text-4xl font-semibold">Jha</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Tabs } from 'antd';
+import Header from "../../components/Header/Header";
+import AdminIntro from "./AdminIntro";
+import AdminAbout from "./AdminAbout";
 
+const {TabPane} = Tabs
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="mt-5 p-5">
+        <Tabs defaultActiveKey="1">
+          <TabPane tab='Intro' key='1'>
+            <AdminIntro />
+          </TabPane>
+          <TabPane tab='About' key='2'>
+            <AdminAbout />
+          </TabPane>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
