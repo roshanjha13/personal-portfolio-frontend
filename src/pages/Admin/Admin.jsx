56e71c423bf6435973bb1fe5eab2471d494d7961
roshanjha31 <jharoshan618@gmail.com>
2023-10-23 11:26:58 +0530

    form created in admin dashboard intro section

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import { Tabs } from 'antd';
+import { Tabs } from "antd";
 import Header from "../../components/Header/Header";
 import AdminIntro from "./AdminIntro";
 import AdminAbout from "./AdminAbout";
+import { useSelector } from "react-redux";
 
-const {TabPane} = Tabs
+const { TabPane } = Tabs;
 const Admin = () => {
+  const { portfolioData } = useSelector((state) => state.root);
   return (
     <div>
       <Header />
-      <div className="mt-5 p-5">
-        <Tabs defaultActiveKey="1">
-          <TabPane tab='Intro' key='1'>
-            <AdminIntro />
-          </TabPane>
-          <TabPane tab='About' key='2'>
-            <AdminAbout />
-          </TabPane>
-        </Tabs>
-      </div>
+      {portfolioData && (
+        <div className="mt-5 p-5">
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Intro" key="1">
+              <AdminIntro />
+            </TabPane>
+            <TabPane tab="About" key="2">
+              <AdminAbout />
+            </TabPane>
+          </Tabs>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/Admin/AdminIntro.jsx
+++ src/pages/Admin/AdminIntro.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import { Form } from 'antd'
+import { useSelector } from 'react-redux'
 const AdminIntro = () => {
+
+  const {portfolioData} = useSelector((state)=>state.root)
+
+  const onFinish = (values)=>{
+    console.log(values);
+  }
   return (
-    <div>AdminIntro</div>
+    <div className=''>
+      <Form onFinish={onFinish} layout='vertical' initialValues={portfolioData?.intro}>
+        <Form.Item name='welcomeText' label='Welcome Text'>
+          <input placeholder=' Welcome Text'  />
+        </Form.Item>
+        <Form.Item name='firstName' label='First Name'>
+          <input placeholder=' first name' />
+        </Form.Item>
+        <Form.Item name='middleName' label='Middle Name'>
+          <input placeholder=' middle name' />
+        </Form.Item>
+        <Form.Item name='lastName' label='Last Name'>
+          <input placeholder=' last name' />
+        </Form.Item>
+        <Form.Item name='caption' label='Caption'>
+          <input placeholder=' caption' />
+        </Form.Item>
+        <Form.Item name='description' label='Description'>
+          <textarea placeholder=' description' />
+        </Form.Item>
+        <div className="flex justify-end w-full">
+          <button className='px-5 py-2 text-white bg-red-700 rounded-full hover:bg-lime-600' type='submit'>Save</button>
+        </div>
+      </Form>
+    </div>
   )
 }
 
