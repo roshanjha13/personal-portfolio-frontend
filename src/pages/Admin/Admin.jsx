26af5b86acb5e8c676e5b95d0079a93e9718d075
roshanjha31 <jharoshan618@gmail.com>
2023-10-23 17:16:05 +0530

    Admin about update api is integrated with frontend admin

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -3,6 +3,10 @@ import { Tabs } from "antd";
 import Header from "../../components/Header/Header";
 import AdminIntro from "./AdminIntro";
 import AdminAbout from "./AdminAbout";
+import AdminExperiences from './AdminExperiences'
+import AdminProject from './AdminProject'
+import AdminContact from './AdminContact'
+import AdminEducation from "./AdminEducation";
 import { useSelector } from "react-redux";
 
 const { TabPane } = Tabs;
@@ -19,6 +23,18 @@ const Admin = () => {
             </TabPane>
             <TabPane tab="About" key="2">
               <AdminAbout />
+            </TabPane>
+            <TabPane tab="Experiences" key="3">
+              <AdminExperiences/>
+            </TabPane>
+            <TabPane tab="Contact" key="4">
+              <AdminContact />
+            </TabPane>
+            <TabPane tab="Project" key="5">
+              <AdminProject />
+            </TabPane>
+            <TabPane tab="Education" key="6">
+              <AdminEducation />
             </TabPane>
           </Tabs>
         </div>
--- src/pages/Admin/AdminAbout.jsx
+++ src/pages/Admin/AdminAbout.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { Form } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { HideLoading, ShowLoading } from "../../redux/rootSlice";
+import axios from "axios";
+import { message } from "antd";
 
 const AdminAbout = () => {
-  return (
-    <div>AdminAbout</div>
-  )
-}
+  const { portfolioData } = useSelector((state) => state.root);
 
-export default AdminAbout
+  const dispatch = useDispatch();
+
+  const onFinish = async (values) => {
+    try {
+      const tempSkills = values.skills.split(",")
+      values.skills = tempSkills;
+      dispatch(ShowLoading());
+      const response = await axios.put(
+        "http://localhost:3000/api/update-about",
+        {
+          ...values,
+          _id: portfolioData.about._id,
+        }
+      );
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading())
+      message.error(response.data.message);
+    }
+  };
+  return (
+    <div>
+      <Form
+        onFinish={onFinish}
+        layout="vertical"
+        initialValues={{
+          ...portfolioData.about,
+          skills:portfolioData.about.skills.join(' ,'),
+        }}
+      >
+        <Form.Item name="lottieUrl" label="Lottie Url">
+          <input placeholder=" lottieUrl" />
+        </Form.Item>
+        <Form.Item name="description1" label="Description1">
+          <textarea placeholder=" Description1" />
+        </Form.Item>
+        <Form.Item name="description2" label="Description2">
+          <textarea placeholder=" Description2" />
+        </Form.Item>
+        <Form.Item name="skills" label="Skills">
+          <textarea placeholder="Skills" />
+        </Form.Item>
+        <div className="flex justify-end w-full">
+          <button
+            className="px-5 py-2 text-white bg-red-700 rounded-full hover:bg-lime-600"
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default AdminAbout;
