b847aa90e46315c81877cbbbbbfc8bd9dc282aa3
roshanjha31 <jharoshan618@gmail.com>
2023-10-23 13:41:52 +0530

    admin intro section integrated in frontend

--- src/components/Header/Loader.jsx
+++ src/components/Header/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className='h-screen flex items-center justify-center fixed inset-0 bg-primary'>
+    <div className='h-screen flex items-center justify-center fixed inset-0 bg-primary z-[100]'>
         <div className="flex gap-5 text-4xl font-semibold sm:text-3xl">
             <h1 className="text-secondary r">R</h1>
             <h1 className="text-white k">K</h1>
--- src/pages/Admin/AdminIntro.jsx
+++ src/pages/Admin/AdminIntro.jsx
@@ -1,40 +1,72 @@
-import React from 'react'
-import { Form } from 'antd'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Form } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { HideLoading, ShowLoading } from "../../redux/rootSlice";
+import axios from "axios";
+import { message } from "antd";
+
 const AdminIntro = () => {
+  const { portfolioData } = useSelector((state) => state.root);
 
-  const {portfolioData} = useSelector((state)=>state.root)
+  const dispatch = useDispatch();
 
-  const onFinish = (values)=>{
-    console.log(values);
-  }
+  const onFinish = async (values) => {
+    try {
+      dispatch(ShowLoading());
+      const response = await axios.put(
+        "http://localhost:3000/api/update-intro",
+        {
+          ...values,
+          _id: portfolioData.intro._id,
+        }
+      );
+      dispatch(HideLoading());
+      if (response.data.success) {
+        message.success(response.data.message);
+      } else {
+        message.error(response.data.message);
+      }
+    } catch (error) {
+      dispatch(HideLoading())
+      message.error(response.data.message);
+    }
+  };
   return (
-    <div className=''>
-      <Form onFinish={onFinish} layout='vertical' initialValues={portfolioData?.intro}>
-        <Form.Item name='welcomeText' label='Welcome Text'>
-          <input placeholder=' Welcome Text'  />
+    <div>
+      <Form
+        onFinish={onFinish}
+        layout="vertical"
+        initialValues={portfolioData?.intro}
+      >
+        <Form.Item name="welcomeText" label="Welcome Text">
+          <input placeholder=" Welcome Text" />
         </Form.Item>
-        <Form.Item name='firstName' label='First Name'>
-          <input placeholder=' first name' />
+        <Form.Item name="firstName" label="First Name">
+          <input placeholder=" first name" />
         </Form.Item>
-        <Form.Item name='middleName' label='Middle Name'>
-          <input placeholder=' middle name' />
+        <Form.Item name="middleName" label="Middle Name">
+          <input placeholder=" middle name" />
         </Form.Item>
-        <Form.Item name='lastName' label='Last Name'>
-          <input placeholder=' last name' />
+        <Form.Item name="lastName" label="Last Name">
+          <input placeholder=" last name" />
         </Form.Item>
-        <Form.Item name='caption' label='Caption'>
-          <input placeholder=' caption' />
+        <Form.Item name="caption" label="Caption">
+          <input placeholder=" caption" />
         </Form.Item>
-        <Form.Item name='description' label='Description'>
-          <textarea placeholder=' description' />
+        <Form.Item name="description" label="Description">
+          <textarea placeholder=" description" />
         </Form.Item>
         <div className="flex justify-end w-full">
-          <button className='px-5 py-2 text-white bg-red-700 rounded-full hover:bg-lime-600' type='submit'>Save</button>
+          <button
+            className="px-5 py-2 text-white bg-red-700 rounded-full hover:bg-lime-600"
+            type="submit"
+          >
+            Save
+          </button>
         </div>
       </Form>
     </div>
-  )
-}
+  );
+};
 
-export default AdminIntro
+export default AdminIntro;
